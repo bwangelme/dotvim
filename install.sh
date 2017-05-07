b401f29c850124b4cd0945459a296cde933a6abe
bwangel23 <bwangel.me@gmail.com>
2017-05-07 21:17:00 +0800

    feat(server): add vimrc-server config

--- install.sh
+++ install.sh
@@ -19,38 +19,66 @@ lnif() {
     fi
 }
 
+install-server () {
+    echo "Step1: backing up current vim config"
+    today=`date +%Y%m%d`
+    for i in $HOME/.vimrc-server; do
+        if [[ -L $i ]];then
+            unlink $i ;
+        else
+            [ -e $i ] && mv $i $i.$today;
+        fi
+    done
 
-echo "Step1: backing up current vim config"
-today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do
-    if [[ -L $i ]];then
-        unlink $i ;
-    else
-        [ -e $i ] && mv $i $i.$today;
-    fi
-done
+    echo "Step2: setting up symlinks"
+    lnif $CURRENT_DIR/vimrc-server $HOME/.vimrc-server
+}
 
-echo "Step2: setting up symlinks"
-lnif $CURRENT_DIR/vimrc $HOME/.vimrc
-lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
-lnif "$CURRENT_DIR/" "$HOME/.vim"
+install () {
+    echo "Step1: backing up current vim config"
+    today=`date +%Y%m%d`
+    for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do
+        if [[ -L $i ]];then
+            unlink $i ;
+        else
+            [ -e $i ] && mv $i $i.$today;
+        fi
+    done
 
-echo "Step3: update/install plugins using Vim-plug"
-system_shell=$SHELL
-export SHELL="/bin/sh"
-vim -u $HOME/.vimrc +PlugInstall! +PlugClean! +qall
-export SHELL=$system_shell
+    echo "Step2: setting up symlinks"
+    lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+    lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
+    lnif "$CURRENT_DIR/" "$HOME/.vim"
+
+    echo "Step3: update/install plugins using Vim-plug"
+    system_shell=$SHELL
+    export SHELL="/bin/sh"
+    vim -u $HOME/.vimrc +PlugInstall! +PlugClean! +qall
+    export SHELL=$system_shell
 
 
-echo "Step4: compile YouCompleteMe"
-echo "It will take a long time, just be patient!"
-echo "If error,you need to compile it yourself"
-echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
+    echo "Step4: compile YouCompleteMe"
+    echo "It will take a long time, just be patient!"
+    echo "If error,you need to compile it yourself"
+    echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
 
-cd $CURRENT_DIR/bundle/YouCompleteMe/
-git submodule update --init --recursive
-$PYTHON install.py --clang-completer
+    cd $CURRENT_DIR/bundle/YouCompleteMe/
+    git submodule update --init --recursive
+    $PYTHON install.py --clang-completer
 
-echo "Install Done!"
-echo "Please check the Python interpreter for YouCompleteMe manually"
-echo "let g:ycm_server_python_interpreter = '$PYTHON'"
+    echo "Install Done!"
+    echo "Please check the Python interpreter for YouCompleteMe manually"
+    echo "let g:ycm_server_python_interpreter = '$PYTHON'"
+}
+
+case "$1" in
+    server)
+        install-server || exit 1
+        ;;
+    pc)
+        install || exit 1
+        ;;
+    *)
+        echo "Usage: $0 {server|pc}"
+        ;;
+esac
