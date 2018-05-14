4623589fa8ac9b2c6006292769b8adfc8661d20c
bwangel23 <bwangel.me@gmail.com>
2018-05-14 19:05:44 +0800

    Delete the ycm install

--- install.sh
+++ install.sh
@@ -57,18 +57,7 @@ install-nvim () {
     nvim -u $HOME/.config/nvim/init.vim +PlugInstall! +PlugClean! +qall
     export SHELL=$system_shell
 
-    echo "Step4: compile YouCompleteMe"
-    echo "It will take a long time, just be patient!"
-    echo "If error,you need to compile it yourself"
-    echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
-
-    cd $CURRENT_DIR/bundle/YouCompleteMe/
-    git submodule update --init --recursive
-    $PYTHON install.py --clang-completer
-
     echo "Install Done!"
-    echo "Please check the Python interpreter for YouCompleteMe manually"
-    echo "let g:ycm_server_python_interpreter = '$PYTHON'"
 }
 
 case "$1" in
